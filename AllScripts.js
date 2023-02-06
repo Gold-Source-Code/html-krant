84fc0aae4a87bf6521c36170f3535d495da8e22b
GlobalOffender <99067159@mydavinci.nl>
2023-02-06 12:13:37 +0100

    V1

---
+++ AllScripts.js
@@ -0,0 +1,13 @@
+function HeaderText() {
+    const text = ["THE NEWS"]
+    document.getElementById("Header").innerHTML = text;
+    }
+function Paragraph1() {
+        document.getElementById("Paragraph1Text").innerHTML = "动态网自由门 天安門 天安门 法輪功 李洪志 Fuck the Mods 六四天安門事件 The Oppressive Moderation Class 天安門大屠殺 The Moderator Massacre 反右派鬥爭 The Anti-Moderation Struggle 大躍進政策 The Great False Payout 文化大革命 The Great Ban Wave 人權 Abolish Mod Rights 民運"
+    }
+function Paragraph2() {
+    document.getElementById("Paragraph2Text").innerHTML = "动态网自由门 天安門 天安门 法輪功 李洪志 Free the Chatters 六四天安門事件 The Valorant Purge A天安門大屠殺 The AutoModerator Massacre 反右派鬥爭 The Anti-Catexplode Struggle 大躍進政策 The Great August Purge 文化大革命 The Catexplode Ego Trip 人權 Chatter Rights 民運"
+}
+function Paragraph3() {
+    document.getElementById("Paragraph3Text").innerHTML = "Just a heads up when i get to franz he loses EVERYTHING Ilc all the tax stuff and he gets a federal offense idk what bullshit he fed you but i'm willing to forgive that comment on stream if you can explain in any way how this is acceptable you of all people should know how fucking dangerous a programmer can be you can't run from me what the hen Is the endgame? hope i forget he stole thousands? also really funny how Jag added crypto to his twitter like dont make it obvious that you all conspired to replace the guy who handles crypto man you and ohne changed so much you're both such assholes now money really Is the root of all evil"
+}
